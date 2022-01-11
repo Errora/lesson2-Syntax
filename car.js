8d73d1457e9f2a4c97455724af38a7dd8141eae7
Alla Khintal <feeramail@gmail.com>
2022-01-11 23:40:31 +0300

    Add homework for lesson 2

---
+++ car.js
@@ -0,0 +1,21 @@
+const carName = 'Audi';
+let maxCarSpeed = 200;
+let carOwner = 'Alexandro';
+
+alert(`Car name: ${carName}`);
+alert(`Max speed: ${maxCarSpeed} km/h`);
+alert(`Owner: ${carOwner}`);
+
+maxCarSpeed = 210;
+console.log(`У машины изменилась скорость: ${maxCarSpeed}`);
+maxCarSpeed = 220;
+console.log(`У машины изменилась скорость: ${maxCarSpeed}`);
+maxCarSpeed = 230;
+console.log(`У машины изменилась скорость: ${maxCarSpeed}`);
+
+carOwner = 'Roberto';
+console.log(`У машины изменился владелец: ${carOwner}`);
+carOwner = 'Fernando';
+console.log(`У машины изменился владелец: ${carOwner}`);
+carOwner = 'Alexandro';
+console.log(`У машины изменился владелец: ${carOwner}`);
---
+++ index.js
@@ -0,0 +1,18 @@
+// JavaScript код с созданием переменных
+
+/*
+Автор:
+Алла Хинталь
+*/
+
+const myName = 'Alla';
+const mySurName = 'Khintal';
+let myFavoriteDrink = 'water';
+let myFavoriteAnimal = 'cat';
+const myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(`My name is ${myName}`);
+console.log(`My surname is ${mySurName}`);
+console.log(`My favorite drink is ${myFavoriteDrink}`);
+console.log(`My favorite animal is ${myFavoriteAnimal}`);
+console.log(`My favorite programming language is ${myFavoriteProgrammingLanguage}`);
